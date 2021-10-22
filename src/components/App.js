6a0b035d7828556fe00550ba24ce515bdb64f4bd
Ezen <82026690+ezengaston@users.noreply.github.com>
2021-10-22 14:38:43 +0100

    store items added

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,39 @@
 import React from "react";
+import Store from "./Store";
+import items from "../items.json";
 
 function App() {
-  return;
+  return (
+    <>
+      <header className="text-gray-700 body-font">
+        <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center justify-center">
+          <nav className="flex flex-wrap items-center text-base justify-center">
+            <a className="mr-5 hover:text-gray-900" href="">
+              Home
+            </a>
+            <a
+              className="mr-5 border-b-2 border-blue-500 hover:text-gray-900"
+              href=""
+            >
+              Store
+            </a>
+            <a className="mr-5 hover:text-gray-900" href="">
+              Team
+            </a>
+          </nav>
+        </div>
+      </header>
+      <section className="text-gray-700 body-font">
+        <div className="container px-5 py-24 mx-auto">
+          <div className="flex flex-wrap -m-4">
+            {items.map((item) => (
+              <Store key={item.id} item={item} />
+            ))}
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Store.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+export default function Store(props) {
+  const imageSource = `https://dummyimage.com/420x260/${props.item.imageColor}/${props.item.imageColor}`;
+  return (
+    <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+      <div className="block relative h-48 rounded overflow-hidden">
+        <img
+          alt="ecommerce"
+          className="object-cover object-center w-full h-full block"
+          src={imageSource}
+        />
+      </div>
+      <div className="mt-4 flex items-end justify-between">
+        <div>
+          <h3 className="text-gray-500 text-xs tracking-widest title-font uppercase mb-1">
+            {props.item.category}
+          </h3>
+          <h2 className="text-gray-900 title-font text-lg font-medium">
+            {props.item.name}
+          </h2>
+          <p className="mt-1">{props.item.priceCents}</p>
+        </div>
+        <button className="text-white py-2 px-4 text-xl bg-blue-500 rounded hover:bg-blue-700">
+          Add To Cart
+        </button>
+      </div>
+    </div>
+  );
+}
